3dfa03a3d2fef57f9f47d9aa5be50313a883a293
Italo Paixão Gomes <italo.gomes@mblabs.com.br>
2023-04-28 20:05:11 -0300

    third step

--- Dollar.java
+++ Dollar.java
@@ -2,8 +2,8 @@ class Dollar extends Money {
   Dollar(int amount) {
     this.amount= amount;
   }
-  
-  Dollar times(int multiplier) {
+
+  Money times(int multiplier)  {
     return new Dollar(amount * multiplier);
-  }
+  }	
 }
