a84fff3765f63d5a6415ba6966feb8ed7fcd7821
Italo Paixão Gomes <italo.gomes@mblabs.com.br>
2023-04-28 20:34:53 -0300

    fourth step

--- Money.java
+++ Money.java
@@ -1,5 +1,6 @@
 abstract class Money  {
-  protected int amount;
+  private String currency; 
+  protected String currency;
   
   public boolean equals(Object object) {
     Money money = (Money) object;
@@ -8,11 +9,20 @@ abstract class Money  {
 
   abstract Money times(int multiplier);  
 
-  static Money dollar(int amount)  {
-    return new Dollar(amount);
+  String currency() {
+    return currency;
   }
-  
+
+  static Money dollar(int amount)  {
+    return new Dollar(amount, "USD");
+  }
+
   static Money franc(int amount) {
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
+  }
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
   }
 }
